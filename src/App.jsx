29102f32a39ad5422dbb040d29cdb03ee5298c62
lukmanxpert <lukmanmiah2004@gmail.com>
2025-11-26 04:46:20 +0600

    make some func in app

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,40 @@ import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import { ToastContainer } from "react-toastify";
+import { useSelector } from "react-redux";
+import Dashboard from "./components/Dashboard";
+import Orders from "./components/Orders";
+import Users from "./components/Users";
+import Profile from "./components/Profile";
+import Products from "./components/Products";
 
 function App() {
+
+  const { openedComponent } = useSelector(state => state.extra)
+  const { user, isAuthenticated } = useSelector(state => state.auth)
+
+  const renderDashboardContent = () => {
+    switch (openedComponent) {
+      case "Dashboard":
+        <Dashboard />
+        break;
+      case "Orders":
+        <Orders />
+        break;
+      case "Users":
+        <Users />
+        break;
+      case "Profile":
+        <Profile />
+        break;
+      case "Products":
+        <Products />
+        break;
+
+      default:
+        return <Dashboard />
+    }
+  }
 
   return (
     <Router>
@@ -18,7 +50,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/password/forgot" element={<ForgotPassword />} />
         <Route path="/password/reset/:token" element={<ResetPassword />} />
-        
+
         {/* Protected Admin Route */}
         <Route
           path="/"
