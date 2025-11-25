da6cbea7d0b398bb68a1f55a2cfd71e3d34f0b87
lukmanxpert <lukmanmiah2004@gmail.com>
2025-11-26 05:08:08 +0600

    added auth slice reducers

--- src/store/slices/authSlice.js
+++ src/store/slices/authSlice.js
@@ -7,7 +7,87 @@ const authSlice = createSlice({
     user: null,
     isAuthenticated: false,
   },
-  reducers: {},
+  reducers: {
+    loginRequest: (state) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    loginFailed: (state) => {
+      state.loading = false;
+    },
+    getUserRequest: (state) => {
+      state.loading = true;
+    },
+    getUserSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    getUserFailed: (state) => {
+      state.loading = false;
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+    logoutRequest: (state) => {
+      state.loading = true;
+    },
+    logoutSuccess: (state) => {
+      state.loading = false;
+      state.user = null;
+      state.isAuthenticated = false;
+    },
+    logoutFailed: (state) => {
+      state.loading = false;
+    },
+    forgotPasswordRequest: (state) => {
+      state.loading = true;
+    },
+    forgotPasswordSuccess: (state) => {
+      state.loading = false;
+    },
+    forgotPasswordFailed: (state) => {
+      state.loading = false;
+    },
+    resetPasswordRequest: (state) => {
+      state.loading = true;
+    },
+    resetPasswordSuccess: (state, action) => {
+      state.loading = false;
+      state.isAuthenticated = true;
+      state.user = action.payload;
+    },
+    resetPasswordFailed: (state) => {
+      state.loading = false;
+    },
+    updateProfileRequest: (state) => {
+      state.loading = true;
+    },
+    updateProfileSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+    },
+    updateProfileFailed: (state) => {
+      state.loading = false;
+    },
+    updatePasswordRequest: (state) => {
+      state.loading = true;
+    },
+    updatePasswordSuccess: (state) => {
+      state.loading = false;
+    },
+    updatePasswordFailed: (state) => {
+      state.loading = false;
+    },
+    resetAuthSlice: (state) => {
+      state.loading = false;
+      // state.user = state.user;
+      // state.isAuthenticated = state.isAuthenticated;
+    },
+  },
 });
 
 export default authSlice.reducer;
