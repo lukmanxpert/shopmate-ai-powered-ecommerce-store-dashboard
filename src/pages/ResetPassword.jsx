01dfe8921c0e241b1c7e145ab931107847a67eb9
lukmanxpert <lukmanmiah2004@gmail.com>
2025-11-26 04:08:47 +0600

    project initialized and setup

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useParams } from "react-router-dom";
+
+const ResetPassword = () => {
+  return <></>;
+};
