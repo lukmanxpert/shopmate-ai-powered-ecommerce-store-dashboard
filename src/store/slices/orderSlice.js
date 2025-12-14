8e3a3e5f4c246dd99ce30628daaa17f11cf42ed1
lukmanxpert <lukmanmiah2004@gmail.com>
2025-12-15 05:21:55 +0600

    added order handler func

--- src/store/slices/orderSlice.js
+++ src/store/slices/orderSlice.js
@@ -1,4 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../lib/axios";
+import { toast } from "react-toastify";
+
+export const fetchAllOrders = createAsyncThunk(
+  "orders/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const { data } = await axiosInstance.get("/order/admin/getall");
+      return data.orders;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "Failed to fetch orders."
+      );
+    }
+  }
+);
+
+export const updateOrderStatus = createAsyncThunk(
+  "orders/updateStatus",
+  async ({ orderId, status }, thunkAPI) => {
+    try {
+      const { data } = await axiosInstance.put(
+        `/order/admin/update/${orderId}`,
+        { status }
+      );
+      toast.success(data.message || "Order status updated successfully.");
+      return data.updatedOrder;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "Failed to update order status."
+      );
+    }
+  }
+);
+
+export const deleteOrder = createAsyncThunk(
+  "orders/delete",
+  async (orderId, thunkAPI) => {
+    try {
+      const { data } = await axiosInstance.delete(
+        `/order/admin/delete/${orderId}`
+      );
+      toast.success(data.message || "Order delete successfully.");
+      return orderId;
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Failed to update order.");
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "Failed to update order."
+      );
+    }
+  }
+);
 
 const orderSlice = createSlice({
   name: "order",
