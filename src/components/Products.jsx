50f816197d6d02696989ba84e41954d8c2fd1d48
lukmanxpert <lukmanmiah2004@gmail.com>
2025-12-27 16:56:32 +0600

    added func part in products page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,7 +7,35 @@ import UpdateProductModal from "../modals/UpdateProductModal";
 import ViewProductModal from "../modals/ViewProductModal";
 
 const Products = () => {
-  return <></>;
+  const [selectedProduct, setSelectedProducts] = useState(null)
+  const [maxPage, setMaxPage] = useState(null)
+  const [page, setPage] = useState(1)
+
+  const dispatch = useDispatch()
+
+  const { isViewProductModalOpened, isCreateProductModalOpened, isUpdateProductModalOpened } = useSelector(state => state.extra)
+  const { loading, products, totalProducts } = useSelector(state => state.product)
+
+  useEffect(() => {
+    // fetch all products
+  }, [dispatch, page])
+
+  useEffect(() => {
+    if (totalProducts !== undefined) {
+      const newMax = Math.ceil(totalProducts / 10)
+      setMaxPage(newMax || 1)
+    }
+  }, [totalProducts])
+
+  useEffect(() => {
+    if (maxPage && page > maxPage) {
+      setPage(maxPage)
+    }
+  }, [maxPage, page])
+
+  return <>
+  
+  </>;
 };
 
 export default Products;
